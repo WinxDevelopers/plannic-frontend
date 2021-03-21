2d6ec99c1670e72fccc68e8849562664af80f757
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2021-03-21 03:47:58 -0300

    Update login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -18,18 +18,10 @@ export class LoginComponent implements OnInit {
   redirectTo: string = '';
   roles: string[] = [];
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  senhaFormControl = new FormControl('', [
-    Validators.required
-  ]);
-
   constructor(
     private authService: LoginService,
     private router: Router,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute) { }
   email: String;
   senha: String;
 
@@ -44,78 +36,97 @@ export class LoginComponent implements OnInit {
     }
   }
 
+
+  valEmail: boolean = true;
+  valCampos: boolean = true;
   onSubmit(): void {
+    this.valEmail = true;
+    this.valCampos = true;
     const { email, password } = this.form;
     const Toast = Swal.mixin({
       toast: true,
       position: 'top-end',
       showConfirmButton: false,
-      timer: 3000,
+      timer: 4000,
       didOpen: (toast) => {
         toast.addEventListener('mouseenter', Swal.stopTimer)
         toast.addEventListener('mouseleave', Swal.resumeTimer)
       }
     })
-    this.authService.login(email, password).subscribe(
-      data => {
-        this.dadosLogin = JSON.parse(data);
-        localStorage.setItem('idUsuario', this.dadosLogin.idUsuario)
-        localStorage.setItem('nome', this.dadosLogin.nome)
-        localStorage.setItem('token', this.dadosLogin.token)
-        this.router.navigate(['/dashboard/calendario']);
-      },
-      err => {
-        this.errorMessage = err.error.message;
-        switch (err.status) {
-          case 500:
-            if (localStorage.getItem("lang") != "en") {
-              Toast.fire({
-                icon: 'error',
-                title: 'Você digitou algo errado'
-              })
-            } else {
-              Toast.fire({
-                icon: 'error',
-                title: 'You typed something wrong'
-              })
-            }
-            break;
-          case 401:
-            if (localStorage.getItem("lang") != "en") {
-              Toast.fire({
-                icon: 'error',
-                title: 'Verifique seu e-mail'
-              })
-            } else {
-              Toast.fire({
-                icon: 'error',
-                title: 'Verify your e-mail'
-              })
-            }
-            break;
 
-          default:
-            if (localStorage.getItem("lang") != "en") {
-              Toast.fire({
-                icon: 'error',
-                title: 'Ocorreu um erro'
-              })
-            } else {
-              Toast.fire({
-                icon: 'error',
-                title: 'An error has occurred'
-              })
-            }
-            break;
+    let emailFormControl = new FormControl(email, [
+      Validators.required,
+      Validators.email,
+    ]);
+    let senhaFormControl = new FormControl(password, [
+      Validators.required
+    ]);
+
+    if (emailFormControl.valid && senhaFormControl.valid) {
+      this.authService.login(email, password).subscribe(
+        data => {
+          this.dadosLogin = JSON.parse(data);
+          localStorage.setItem('idUsuario', this.dadosLogin.idUsuario)
+          localStorage.setItem('nome', this.dadosLogin.nome)
+          localStorage.setItem('token', this.dadosLogin.token)
+          this.router.navigate(['/dashboard/calendario']);
+        },
+        err => {
+          this.errorMessage = err.error.message;
+          switch (err.status) {
+            case 500:
+              if (localStorage.getItem("lang") != "en") {
+                Toast.fire({
+                  icon: 'error',
+                  title: 'Você digitou algo errado'
+                })
+              } else {
+                Toast.fire({
+                  icon: 'error',
+                  title: 'You typed something wrong'
+                })
+              }
+              break;
+            case 401:
+              if (localStorage.getItem("lang") != "en") {
+                Toast.fire({
+                  icon: 'error',
+                  title: 'Verifique seu e-mail'
+                })
+              } else {
+                Toast.fire({
+                  icon: 'error',
+                  title: 'Verify your e-mail'
+                })
+              }
+              break;
+
+            default:
+              if (localStorage.getItem("lang") != "en") {
+                Toast.fire({
+                  icon: 'error',
+                  title: 'Ocorreu um erro'
+                })
+              } else {
+                Toast.fire({
+                  icon: 'error',
+                  title: 'An error has occurred'
+                })
+              }
+              break;
+          }
         }
+      );
+    } else {
+      if (emailFormControl.hasError("required") || senhaFormControl.hasError("required")) {
+        this.valCampos = false;
+        return
       }
-    );
-  }
+      if (emailFormControl.hasError("email")) {
+        this.valEmail = false;
+        return
+      }
+    }
 
-  isNull() {
-    if (this.form.email == null && this.form.senha == null)
-      return true
-    else
-      return false
   }
 }
