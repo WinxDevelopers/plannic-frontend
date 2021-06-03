957966971771a642bdc88ef4cd9966446aab4d76
Vitor Massao Sugai <massao.sg@gmail.com>
2021-06-03 20:24:31 -0300

    Security Header

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -10,9 +10,15 @@ export class LoginService {
   public LOGIN_SERVICE_URL = `${environment.API_URL}`;
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-type': 'application/json' }),
+    headers: new HttpHeaders({ 'Content-type': 'application/json',
+                               'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+                               'Content-Security-Policy': 'Content-Security-Policy: default-src https://plannic-backend.herokuapp.com; default-src http://localhost:8080',
+                               'X-Frame-Options': 'SAMEORIGIN',
+                               'X-Content-Type-Options': 'nosniff always',
+                               'Referrer-Policy': 'no-referrer',
+                               'Permissions-Policy': 'geolocation=(self "https://plannic-backend.herokuapp.com" "http://localhost:8080")' }),
     responseType: 'text' as 'json'
-  }
+}
   constructor(private http: HttpClient) { }
 
  
--- src/app/service/notaMateria.service.ts
+++ src/app/service/notaMateria.service.ts
@@ -12,10 +12,14 @@ export class NotaMateriaService {
     public IdUsuario = localStorage.getItem('idUsuario')
 
     httpOptions = {
-        headers: new HttpHeaders({
-            'Content-type': 'application/json',
-            'Authorization': `Bearer ${this.token}`
-        }),
+        headers: new HttpHeaders({ 'Content-type': 'application/json',
+                                   'Authorization': `Bearer ${this.token}`,
+                                   'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+                                   'Content-Security-Policy': 'Content-Security-Policy: default-src https://plannic-backend.herokuapp.com; default-src http://localhost:8080',
+                                   'X-Frame-Options': 'SAMEORIGIN',
+                                   'X-Content-Type-Options': 'nosniff always',
+                                   'Referrer-Policy': 'no-referrer',
+                                   'Permissions-Policy': 'geolocation=(self "https://plannic-backend.herokuapp.com" "http://localhost:8080")' }),
         responseType: 'text' as 'json'
     }
     constructor(private http: HttpClient) { }
