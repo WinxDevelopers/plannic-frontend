6e92b302c9db7f0b13b5327369215f1e28c8fc05
Vitor Massao Sugai <massao.sg@gmail.com>
2021-06-04 19:31:56 -0300

    Headers config

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "default-src": ["'self'",'https://plannic-backend.herokuapp.com', 'https://fonts.googleapis.com/', 'https://fonts.gstatic.com'],
+        "img-src": ["'self'", '*.googleapis.com', "data:" ],
+        "script-src": ["'self'"],
+        "style-src": ["'unsafe-inline'", "'self'", "https://fonts.googleapis.com"]
+    }
+}));
+
+app.use((_, res, next) => {
+    res.setHeader(
+      "Permissions-Policy"
+    );
+    next();
+});
 
 app.use(express.static(`${__dirname}/dist/plannic`));
 
