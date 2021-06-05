26fe86df6c4f74e3e87c890ab4f6bdd4a64077f0
Vitor Massao Sugai <massao.sg@gmail.com>
2021-06-04 22:19:53 -0300

    Update Headers

--- server.js
+++ server.js
@@ -1,30 +1,6 @@
 const express = require('express');
 const path = require('path');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true }));
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        "default-src": ["'self'",'https://plannic-back.herokuapp.com', 'https://plannic.herokuapp.com', 'https://fonts.googleapis.com/', 'https://fonts.gstatic.com', 'https://securepubads.g.doubleclick.net/'],
-        "img-src": ["'self'", 'https://plannic.herokuapp.com', '*.googleapis.com', "data:" ],
-        "script-src": ["'self'", 'https://plannic.herokuapp.com', 'https://cdnjs.cloudflare.com'],
-        "style-src": ["'self'", "https://fonts.googleapis.com"]
-    }
-}));
-
-app.use((_, res, next) => {
-    res.setHeader(
-      "Permissions-Policy",
-      'geolocation=(self), microphone=()'
-    );
-    next();
-});
 
 app.use(express.static(`${__dirname}/dist/plannic`));
 
