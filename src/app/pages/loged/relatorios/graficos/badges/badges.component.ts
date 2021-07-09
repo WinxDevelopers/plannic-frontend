4623361610e64873a295f0f0880348e12d553f75
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2021-07-09 00:41:29 -0300

    Update Badges

--- src/app/pages/loged/relatorios/graficos/badges/badges.component.ts
+++ src/app/pages/loged/relatorios/graficos/badges/badges.component.ts
@@ -1,50 +1,48 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GraficosService } from 'src/app/service/graficos.service';
 
 @Component({
   selector: 'badges',
   templateUrl: './badges.component.html'
 })
-export class BadgesComponent implements OnInit{
+export class BadgesComponent implements OnInit {
   constructor(
-    private graficoService:GraficosService
-  ){}
+    private graficoService: GraficosService
+  ) { }
 
-  public nomeUsuario = localStorage.getItem('nome')
   public notaMenor;
   public notaMaior;
   public buscaTutor = false;
   public sejaTutor = false;
-  
+
   ngOnInit() {
-    this.menorNota();
-    this.maiorNota();
+    this.getNotas();
   }
 
-  menorNota() {
+  getNotas() {
     this.graficoService.notaMenor().subscribe(
       (notaMenor) => {
-        if (!notaMenor) {
+        notaMenor = JSON.parse(notaMenor);
+        console.log(notaMenor)
+        if (!notaMenor || notaMenor.length === 0) {
           this.buscaTutor = false;
         } else {
           this.buscaTutor = true;
-          notaMenor = JSON.parse(notaMenor);
-          this.notaMenor = notaMenor;
+          this.notaMenor = notaMenor[0];
         }
-      }
-    )
-  }
 
-  maiorNota() {
-    this.graficoService.notaMaior().subscribe(
-      (notaMaior) => {
-        if (!notaMaior) {
-          this.sejaTutor = false;
-        } else {
-          notaMaior = JSON.parse(notaMaior);
-          this.notaMaior = notaMaior;
-          this.sejaTutor = true;
-        }
+        this.graficoService.notaMaior().subscribe(
+          (notaMaior) => {
+            notaMaior = JSON.parse(notaMaior);
+            console.log(notaMaior)
+            if (!notaMaior || notaMaior.length === 0) {
+              this.sejaTutor = false;
+            } else {
+              this.notaMaior = notaMaior[0];
+              this.sejaTutor = true;
+            }
+          }
+        )
       }
     )
   }
